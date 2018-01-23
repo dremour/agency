51867874503a5897a898d6795cdc2cd44a40714a
Serhii Antonchyk <antonchyk68@gmail.com>
2018-01-23 22:47:14 +0200

    adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
-$(function () {
-
-
+$(document).ready(function() {
+    $('.hamburger-menu').on('click', function(event) {
+        $('.row__hamburger').addClass('active');
+        event.preventDefault();
+        event.stopPropagation();
+    });
+    $('#close').on('click', function () {
+        $('.row__hamburger').removeClass('active');
+    })
 });
 
 
